498add1a4502810ab962958baf19a60338b0790b
Tobias Megies <megies@geophysik.uni-muenchen.de>
2014-07-17 12:47:37 +0200

    py3ify pdas

--- obspy/pdas/core.py
+++ obspy/pdas/core.py
@@ -25,14 +25,18 @@ def isPDAS(filename):
     :rtype: bool
     :return: ``True`` if a PDAS file.
     """
-    with open(filename, "rb") as fh:
-        header_fields = [fh.readline().split()[0] for i_ in xrange(11)]
-    expected_headers = ['DATASET', 'FILE_TYPE', 'VERSION', 'SIGNAL',
-                        'DATE', 'TIME', 'INTERVAL', 'VERT_UNITS',
-                        'HORZ_UNITS', 'COMMENT', 'DATA']
-    if header_fields == expected_headers:
-        return True
-    else:
+    try:
+        with open(filename, "rb") as fh:
+            header_fields = [fh.readline().split()[0].decode()
+                             for i_ in range(11)]
+        expected_headers = ['DATASET', 'FILE_TYPE', 'VERSION', 'SIGNAL',
+                            'DATE', 'TIME', 'INTERVAL', 'VERT_UNITS',
+                            'HORZ_UNITS', 'COMMENT', 'DATA']
+        if header_fields == expected_headers:
+            return True
+        else:
+            return False
+    except:
         return False
 
 
@@ -61,21 +65,21 @@ def readPDAS(filename, **kwargs):
     """
     extra_headers = {}
     with open(filename, "rb") as fh:
-        items = [fh.readline().split() for i_ in xrange(11)]
+        items = [fh.readline().split() for i_ in range(11)]
         data = fh.read()
     for i_ in (0, 1, 2, 3, 7, 8, 9):
-        extra_headers[items[i_][0]] = items[i_][1]
-    month, day, year = items[4][1].split("-")
+        extra_headers[items[i_][0].decode()] = items[i_][1].decode()
+    month, day, year = items[4][1].decode().split("-")
     if UTCDateTime().year > 2050:
         raise NotImplementedError()
     if int(year) < 50:
         year = "20" + year
     else:
         year = "19" + year
-    time = items[5][1]
+    time = items[5][1].decode()
     t = UTCDateTime("%s-%s-%sT%s" % (year, month, day, time))
-    sampling_rate = 1.0 / float(items[6][1])
-    dtype = items[1][1]
+    sampling_rate = 1.0 / float(items[6][1].decode())
+    dtype = items[1][1].decode()
     if dtype.upper() == "LONG":
         data = np.fromstring(data, dtype='int16')
     elif dtype.upper() == "SHORT":
